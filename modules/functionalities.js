8ee6d6a1a7cbe20d4656736d9a023645428143a4
littyofficials@gmail.com <BryteLitty>
2022-11-23 17:04:32 -0800

    fix linter errors

--- modules/functionalities.js
+++ modules/functionalities.js
@@ -1,55 +1,51 @@
 const data = [
-    {
-        name: 'Bryte Litty',
-        score: 98,
-    },
-    {
-        name: 'Blessing Addison',
-        score: 95,
-    },
-    {
-        name: 'Joyce Bruce',
-        score: 90,
-    },
-    {
-        name: 'Juline Creapx',
-        score: 80,
-    }
-]
-
+  {
+    name: 'Bryte Litty',
+    score: 98,
+  },
+  {
+    name: 'Blessing Addison',
+    score: 95,
+  },
+  {
+    name: 'Joyce Bruce',
+    score: 90,
+  },
+  {
+    name: 'Juline Creapx',
+    score: 80,
+  },
+];
 
 const displayScore = () => {
-    const scoreBoard = document.querySelector('.score-board');
-    scoreBoard.innerHTML = '';
-    data.forEach((score) => {
-        const content = `
+  const scoreBoard = document.querySelector('.score-board');
+  scoreBoard.innerHTML = '';
+  data.forEach((score) => {
+    const content = `
         <li class="score-item">${score.name}: ${score.score}</>
-        `
-        scoreBoard.innerHTML += content;
-    })
-}
-
+        `;
+    scoreBoard.innerHTML += content;
+  });
+};
 
 const addScore = () => {
-    const form = document.querySelector('form');
-    const nameInput = document.querySelector('.name');
-    const scoreInput = document.querySelector('.score');
-    const submitBtn = document.querySelector('.submit-btn');
+  const form = document.querySelector('form');
+  const nameInput = document.querySelector('.name');
+  const scoreInput = document.querySelector('.score');
+  const submitBtn = document.querySelector('.submit-btn');
 
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-        const newInput = {
-            name: nameInput.value,
-            score: scoreInput.value
-        }
+    const newInput = {
+      name: nameInput.value,
+      score: scoreInput.value,
+    };
 
-        data.push(newInput);
-        form.reset()
-        displayScore()
-    })
+    data.push(newInput);
+    form.reset();
+    displayScore();
+  });
+};
 
-}
-
-
-export { displayScore, addScore }
+export { displayScore, addScore };
