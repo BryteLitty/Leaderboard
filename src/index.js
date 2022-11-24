4325aadf5108e150f75b9a99fbfb2b0cc6fb59d3
littyofficials@gmail.com <BryteLitty>
2022-11-23 16:48:48 -0800

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import _ from 'lodash';
 import './style.css';
 
 // import key functions
 import { addScore, displayScore } from '../modules/functionalities.js';
 
 const init = () => {
-    displayScore();
-    addScore();
-}
+  displayScore();
+  addScore();
+};
 
 init();
